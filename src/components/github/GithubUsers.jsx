58a138a9d8d89f12be239db2da702bb497ff8a6b
Jounicode <96347521+Jounicrypto@users.noreply.github.com>
2022-02-26 00:25:28 +0100

    commit

--- src/components/github/GithubUsers.jsx
+++ src/components/github/GithubUsers.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-// import { FcApproval } from "react-icons/fc";
-// import { FaGithub } from "react-icons/fa";
+import { FcApproval } from "react-icons/fc";
+
 
 const GithubUsers = ({users}) => {
     return (
@@ -13,22 +13,22 @@ const GithubUsers = ({users}) => {
 
                         users.map((curElem) => {
                         
-                            const { avatar_url, id, login, type } = curElem;
+                            const { avatar_url, id, login, type, respositories, followers, rating } = curElem;
                         return (
                               <div className="col-10 col-md-4 mt-5" key={id}>
                       <div className="card p-2">
                                 <div className="d-flex align-items-center">
                                         <div className="image"> <img src={ avatar_url } className="rounded" width="155" /> </div>
                                     <div className="ml-3 w-100">
-                                            {/* <h4 className="mb-0 mt-0 textLeft">{login} <FcApproval/> </h4> */}
-                                            {/* <span className="text-left">{type }</span> */}
+                                            <h4 className="mb-0 mt-0 textLeft">{login} <FcApproval/> </h4>
+                                            <span className="text-left">{type}</span>
                                         <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
                                                 <div className="d-flex flex-column">
-                                                    <span className="articles">Articles</span> <span className="number1">38</span> </div>
+                                                    <span className="articles">Repositories</span> <span className="number1" src={respositories}>{respositories}</span> </div>
                                                 <div className="d-flex flex-column">
-                                                    <span className="followers">Followers</span> <span className="number2">980</span> </div>
+                                                    <span className="followers">Followers</span> <span className="number2">{followers}</span> </div>
                                                 <div className="d-flex flex-column">
-                                                    <span className="rating">Rating</span> <span className="number3">8.9</span> </div>
+                                                    <span className="rating">Rating</span> <span className="number3">{rating}</span> </div>
                                         </div>
                                       
                                     </div>
